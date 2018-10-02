5430b663120663329f204fcd68c75dc21835fd76
Ryan Moody <ryan@masomenosmedia.com>
2018-10-02 11:57:42 -0700

    Updated ComicBook Detail View to be strongly typed

--- WaMvTech/WaMvTech/Controllers/ComicBooksController.cs
+++ WaMvTech/WaMvTech/Controllers/ComicBooksController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WaMvTech.Models;
 
 namespace WaMvTech.Controllers
 {
@@ -16,18 +17,22 @@ namespace WaMvTech.Controllers
 
         public ActionResult Detail()
         {
-            ViewBag.SeriesTitle = "The Amazing Spider-Man";
-            ViewBag.IssueNumber = 700;
-            ViewBag.Description = "<p>Final issue! Witness the final hour</p>";
-            ViewBag.Artists = new string[]
+            var comicBook = new ComicBook()
             {
-                "Scripts: Dan Slott",
-                "Pencils: Humberto Ramos",
-                "Inks: Victor Olazaba",
-                "Colors: Edgar Delgado",
-                "Letters: Chris Eliopoulos"
+                SeriesTitle = "The Amazing Spider-Man",
+                IssueNumber = 700,
+                DescriptionHtml = "<p>Final issue! Witness the final hour</p>",
+                Artists = new Artist[] 
+                {
+                    new Artist() { Name = "Dan Slott", Role = "Scripts" },
+                    new Artist() { Name = "Humberto Ramos", Role = "Pencils" },
+                    new Artist() { Name = "Victor Olazaba", Role = "Inks" },
+                    new Artist() { Name = "Edgar Delgado", Role = "Colors" },
+                    new Artist() { Name = "Chris Eliopoulos", Role = "Letters" }
+                }
             };
-            return View();            
+
+            return View(comicBook);            
         }
     }
 }
