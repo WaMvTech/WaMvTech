015571b76f6090c443ce7f328cd1db588b0dbc34
Ryan Moody <ryan@masomenosmedia.com>
2018-10-02 18:03:05 -0700

    Changed Default Routing to Index.

--- WaMvTech/WaMvTech/App_Start/RouteConfig.cs
+++ WaMvTech/WaMvTech/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace WaMvTech
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "ComicBooks", action = "Detail", id = UrlParameter.Optional }
+                defaults: new { controller = "ComicBooks", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
